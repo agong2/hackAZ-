1ba5aaac810d039e545ed8c44ceaf24442882bac
Wilson <wilson0398@gmail.com>
2018-01-13 10:10:34 -0700

    initial react native commit

---
+++ client/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+import HomeScreen from './components/HomeScreen.js';
+
+
+const ProfileScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Details Screen</Text>
+  </View>
+);
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+export default App;
---
+++ client/components/HomeScreen.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, Image, StyleSheet } from 'react-native';
+
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+        <View>
+            <Text style={styles.gay}>god fucking dammit</Text>
+            <Image style={styles.dale} source={require('../images/studying.jpg')}/>
+        </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+    gay: {
+        color: 'blue',
+    },
+    dale: {
+        background-repeat: no-repeat;
+        background-position: top center;
+        background-attachment: fixed;
+        background-size: cover;
+    },
+});
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
