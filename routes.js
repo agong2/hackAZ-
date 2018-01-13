62ee6e688b9d6ddf27e660272a7e9a84c3b8d229
Wilson <wilson0398@gmail.com>
2018-01-13 00:45:55 -0700

    added database connection

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const mongojs = require('mongojs');
+var db = mongojs('mongodb://WilXing:studyup@ds147711.mlab.com:47711/studyupprofiles', ['profiles']);
+
 
 router.get('/getProfile', (req,res,next) =>{
 
