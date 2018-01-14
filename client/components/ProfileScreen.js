30690b9f0d9d6337efd5e1d7982762698f9bfb06
Dale Kim <dalekim1109@gmail.com>
2018-01-14 01:08:02 -0700

    trash commit, ignore

--- client/components/ProfileScreen.js
+++ client/components/ProfileScreen.js
@@ -1,51 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { AppRegistry, Text, View, Image, StyleSheet, Button} from 'react-native';
 import {StackNavigator} from 'react-navigation';
+import ReactDOM from 'react-dom';
+import { WithContext as ReactTags } from 'react-tag-input';
 
 
+//export default class ProfileScreen extends React.Component {
+  class ProfileScreen extends React.Component {
 
-export default class HomeScreen extends Component {
-    constructor(props){
-        super(props);
+    constructor (props) {
+      super(props)
+      this.state = {
+        value: '',
+      }
     }
 
-  render() {
-    return (
-        <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-        <View style={{width: 500, height: 200, marginTop: 100}}>
-            <Text style={{fontSize: 70, fontWeight: 'bold', textAlign: 'center'}}> Profile </Text>
-        </View>
-        <View style={{display: 'flex', flexDirection: 'row', }}>
-            <Button
-            title="Create Your Account"
-            onPress={() => navigation.navigate('Details')}
-            color="#841584"
-            />
-        </View>
-        <Autocomplete
-            getItemValue={(item) => item.label}
-            items={[
-              { label: 'FUCK' },
-              { label: 'MY' },
-              { label: 'ASS' }
-            ]}
-            renderItem={(item, isHighlighted) =>
-              <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-                {item.label}
-              </div>
-            }
-            value={value}
-            onChange={(e) => value = e.target.value}
-            onSelect={(val) => value = val}
-            />
-    );
+    render() {
+      return (
+        <ReactAutocomplete
+          items={[
+            { id: 'foo', label: 'foo' },
+            { id: 'bar', label: 'bar' },
+            { id: 'baz', label: 'baz' },
+          ]}
+          shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+          getItemValue={item => item.label}
+          renderItem={(item, highlighted) =>
+            <div
+              key={item.id}
+              style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+            >
+              {item.label}
+            </div>
+          }
+          value={this.state.value}
+          onChange={e => this.setState({ value: e.target.value })}
+          onSelect={value => this.setState({ value })}
+        />
+      )
+    }
   }
-}
 
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
+ReactDOM.render(<MyInput />, document.body)
